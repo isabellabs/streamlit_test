da78d1fbfc0b84edf9186ddc31466795d7f1264a
Isabella Barbosa <30278887+isabellabs@users.noreply.github.com>
2025-08-28 15:59:14 +0000

    Update dataset information and improve formatting in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Testing")
+st.title("Dataset name")
 st.write("Get to know the generation base: Generation purpose, Medical data used, Generative model used.")
 st.warning('Limitations in the medical dataset used for generation may be reflected in the generated data, resulting in underrepresentation, bias and shortcomings cases.', icon=":material/warning:", width="stretch")
 
@@ -24,11 +24,12 @@ real_dataset = """
 |----------------------|--------------------|
 | Description          | Compilation of two publicly available retina fundus photography datasets EyePACS and EyePACS AIRGOS. Each image has an associated text prompt indicating the presence or the lack of a disease. Additional quality indications are provided for each instance. |
 | Data acquisition     | Retina Fundus photography |
-| Target disease(s)    | - Diabetic retinopathy: severity from 0 (healthy) to 4 (proliferative); <br> - Glaucoma: referable (glaucomatous) and no referable glaucoma (healthy) |
-| Data annotation      | Human annotation: <br> - Diabetic Retinopathy: 0-4 <br> - Glaucoma presence: 0-1 <br> - Subjective quality annotation: 0-2| 
-| Spatial coverage     | - USA (patients with different ethnic backgrounds, i.e., African descent,Caucasian, Asian, Latin American and Native American individuals <br> - Indian Subcontinent (unspecified regions) |                
-| Limitations          | - Great variance in image characteristics, which might hamper modelperformance or usability. <br> - Annotation by different specialists might produce inappropriate classlabel classifications. <br> - Some images are unusable due to poor acquisition conditions. <br>  - Glaucoma images might not reflect real world scenarios, as imageswere acquired in the frame of diabetic retinopathy screeningprograms. <br> - No additional information of patients’ age, sex, comorbidities or otherfactors that might influence the diagnosis. |
-| Biases               | - Ungradable images were excluded from the final datasets. <br>  - Annotations may be biased because they were annotated and disagreements were resolved by several specialists.| 
+| Data size            | <ul><li>Total number of images: 88,702</li><li>Diabetic Retinopathy images: 88,702</li><li>Glaucoma images: 24,366</li></ul> |
+| Target disease(s)    | <ul><li>Diabetic retinopathy: severity from 0 (healthy) to 4 (proliferative)</li><li>Glaucoma: referable (glaucomatous) and no referable glaucoma (healthy)</li></ul> |
+| Data annotation      | Human annotation: <ul><li>Diabetic Retinopathy: 0-4</li><li>Glaucoma presence: 0-1</li><li>Subjective quality annotation: 0-2</li></ul> | 
+| Spatial coverage     | <ul><li>USA (patients with different ethnic backgrounds, i.e., African descent, Caucasian, Asian, Latin American and Native American individuals)</li><li>Indian Subcontinent (unspecified regions)</li></ul> |                
+| Limitations          | <ul><li>Great variance in image characteristics, which might hamper model performance or usability.</li><li>Annotation by different specialists might produce inappropriate class label classifications.</li><li>Some images are unusable due to poor acquisition conditions.</li><li>Glaucoma images might not reflect real world scenarios, as images were acquired in the frame of diabetic retinopathy screening programs.</li><li>No additional information of patients’ age, sex, comorbidities or other factors that might influence the diagnosis.</li></ul> |
+| Biases               | <ul><li>Ungradable images were excluded from the final datasets.</li><li>Annotations may be biased because they were annotated and disagreements were resolved by several specialists.</li></ul> | 
 """
 
 st.markdown(real_dataset, unsafe_allow_html=True)
@@ -36,12 +37,13 @@ st.markdown(real_dataset, unsafe_allow_html=True)
 st.header("Generative model used", help="The generative model used to create the synthetic data")
 
 generative_model = """
-| Title                        | SD20 ZSNR|
-|------------------------------|--------------------|
+| Title                        | SD20 ZSNR |
+|------------------------------|-----------|
 | Description                  | Multipurpose Stable Diffusion model trained on Retinal Fundus Images for Diabetic Retinopathy Grading and Glaucoma Classification. |
-| Generation options available | Disease Intensity (one of): <br> - mild/moderate/severe/proliferative diabetic retinopathy <br> - glaucomatous/healthy <br> - no diabetic retinopathy <br> <br> Quality Attributes (one or more of): <br> - contrast (low/medium/high) <br> - brightness (dark/medium/bright) <br> - color (colorless/medium/colorful) <br> - sharpness (blurred/medium/sharp) <br> - perceptual quality (low/medium/high) <br> Following are some examples of prompts used in the generation: “retinal fundus image, high contrast, colorful, sharp” “moderate diabetic retinopathy retinal fundus image, blurred, bright” “glaucomatous retinal fundus image, high perceptual quality, normal|
-| Limitations                  | - Some image quality descriptions conflict with each other during training. <br> - Asking the model to generate images with specific qualitycharacteristics might lead to unwanted behavior. <br> - Model was trained on a large-scale image dataset which is severely unbalanced. <br> - Images with high diabetic retinopathy levels may not be realistic. <br> - Model only accepts English prompts with a fixed structure.|
-| Biases                       | - Model is biased to the specifications/models of the machines used to acquire the images. <br> - Model is biased to the population of the acquired images. <br> - In some cases, image characteristics may not be aligned with real world scenarios. |
+| Generation options available | <ul><li>Disease Intensity (one of):<ul><li>mild/moderate/severe/proliferative diabetic retinopathy</li><li>glaucomatous/healthy</li><li>no diabetic retinopathy</li></ul></li><br><li>Quality Attributes (all):<ul><li>contrast (low/medium/high)</li><li>brightness (dark/medium/bright)</li><li>color (colorless/medium/colorful)</li><li>sharpness (blurred/medium/sharp)</li><li>perceptual quality (low/medium/high)</li></ul></li><br><li>Prompt examples:<ul><li>“retinal fundus image, high contrast, colorful, sharp”</li><li>“moderate diabetic retinopathy retinal fundus image, blurred, bright”</li><li>“glaucomatous retinal fundus image, high perceptual quality, normal”</li></ul></li></ul> |
+| Limitations                  | <ul><li>Some image quality descriptions conflict with each other during training.</li><li>Asking the model to generate images with specific quality characteristics might lead to unwanted behavior.</li><li>Model was trained on a large-scale image dataset which is severely unbalanced.</li><li>Images with high diabetic retinopathy levels may not be realistic.</li><li>Model only accepts English prompts with a fixed structure.</li></ul> |
+| Biases                       | <ul><li>Model is biased to the specifications/models of the machines used to acquire the images.</li><li>Model is biased to the population of the acquired images.</li><li>In some cases, image characteristics may not be aligned with real world scenarios.</li></ul> |
 """
 
 st.markdown(generative_model, unsafe_allow_html=True)
+
